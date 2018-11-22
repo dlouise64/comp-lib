af95a8e61ee4b7ff2feb8ecc51f7e76d43ada6f0
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 15:17:26 +1300

    add props

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const HelloWorld = ({message}) => {
   return <div>Hello {message}</div>
 }
 
 HelloWorld.propTypes = {
+  /** Message to display */
   message: PropTypes.string
 }
+
+HelloWorld.defaultProps = {
+  message: 'World'
+}
+
 export default HelloWorld;
