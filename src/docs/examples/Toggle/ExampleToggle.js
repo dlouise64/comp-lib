81eaff99602fb7d0f90c27cf811b8e0d8318b84d
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 18:39:55 +1300

    remove comment

--- src/docs/examples/Toggle/ExampleToggle.js
+++ src/docs/examples/Toggle/ExampleToggle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Toggle from 'sil-react/Toggle';
 
-// /** A simple Toggle component */
+/** A simple Toggle component */
 
 export default function ExampleToggle() {
   return <Toggle width="150" />
