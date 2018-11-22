e92c36ddffe37d95436c195d61d7bef2170656b9
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 14:47:25 +1300

    start building doc pages

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-function HelloWorld({message}) {
+const HelloWorld = ({message}) => {
   return <div>Hello {message}</div>
+}
+
+HelloWorld.propTypes = {
+  message: PropTypes.string
 }
 export default HelloWorld;
