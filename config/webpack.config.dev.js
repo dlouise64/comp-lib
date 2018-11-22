9c282f4565376297bcda0652f386daefea3e3028
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 15:27:33 +1300

    create alias for our components

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -150,6 +150,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      // allows us to cut path dir length
+      'sil-react': path.resolve(__dirname, '../src/components')
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+import HelloWorld from 'sil-react/HelloWorld/HelloWorld';
 
 /** Custom message */
 
