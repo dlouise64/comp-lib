4d20b2ddeb47e3f086e3de2b40af10182991ae53
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 17:29:27 +1300

    Updates

---
+++ precache-manifest.1eececaee371042e3f96f4001c2d7a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53ae5f7e0a08b3a994ee",
+    "url": "/static/js/main.53ae5f7e.chunk.js"
+  },
+  {
+    "revision": "db0be96572c3620b2f00",
+    "url": "/static/js/1.db0be965.chunk.js"
+  },
+  {
+    "revision": "53ae5f7e0a08b3a994ee",
+    "url": "/static/css/main.ed173386.chunk.css"
+  },
+  {
+    "revision": "db0be96572c3620b2f00",
+    "url": "/static/css/1.cb5c3b30.chunk.css"
+  },
+  {
+    "revision": "654ff0544eae39de24d2086d37af7b8c",
+    "url": "/index.html"
+  }
+];
