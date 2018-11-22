575b41047e160ff841cb7651a5686775875ee026
Dana Iti <dana.iti.work@gmail.com>
2018-11-22 15:23:38 +1300

    add custom message

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HelloWorld from '../../../components/HelloWorld/HelloWorld';
 
+/** Custom message */
+
 export default function ExampleHelloWorld() {
   return <HelloWorld message="Yo!" />
 }
