1152356704df343a19d4ba3b0f92a3fee2286205
Dana Iti <dana.iti.work@gmail.com>
2018-11-28 06:55:54 +1300

    no components

--- src/components/ProgressBar/ProgressBar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import ProgressBar from './ProgressBar';
-
-test('<ProgressBar /> renders correctly', () => {
-  const tree = renderer.create(<ProgressBar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- src/docs/examples/ProgressBar/ExampleProgressBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ProgressBar from 'sil-react/ProgressBar';
-
-/** ProgressBar */
-export default function ExampleProgressBar() {
-  return <ProgressBar />;
-}
