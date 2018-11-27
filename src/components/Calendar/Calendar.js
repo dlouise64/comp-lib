9e786ab5251df6df11365238819327773472ca7d
Dana Iti <dana.iti.work@gmail.com>
2018-11-28 06:37:43 +1300

    calendar component

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './Calendar.css';
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div className="calendar">Calendar</div>
+    );
+  }
+}
+
+Calendar.propTypes = {
+};
+
+Calendar.defaultProps = {
+}
+
+export default Calendar;
---
+++ src/docs/examples/Calendar/ExampleCalendar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Calendar from 'sil-react/Calendar';
+
+/** Calendar */
+export default function ExampleCalendar() {
+  return <Calendar />;
+}
