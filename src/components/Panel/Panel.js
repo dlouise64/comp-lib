4c2b098dbc1d20952f812435931bde3689670a78
Dana Iti <dana.iti.work@gmail.com>
2018-11-28 06:30:30 +1300

    Panel component with snapshot

---
+++ src/components/Panel/Panel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './Panel.css';
+
+class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">Panel</div>
+    );
+  }
+}
+
+Panel.propTypes = {
+};
+
+Panel.defaultProps = {
+}
+
+export default Panel;
