54173e1beb7186fe032ec596179ae519cb5818b7
Dana Iti <dana.iti.work@gmail.com>
2018-11-28 05:56:36 +1300

    start from clean slate

--- src/docs/examples/Toggle/ExampleToggle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Toggle from 'sil-react/Toggle';
-
-/** A simple Toggle component */
-
-export default function ExampleToggle() {
-  return <Toggle width="150" />
-}
