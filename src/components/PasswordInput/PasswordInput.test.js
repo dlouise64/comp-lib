539cbb62ba69426cb87b408ff526ece92fcf332c
Dana Iti <dana.iti.work@gmail.com>
2018-11-28 08:26:07 +1300

    password input snapshot

--- src/components/PasswordInput/PasswordInput.test.js
+++ src/components/PasswordInput/PasswordInput.test.js
@@ -3,6 +3,17 @@ import renderer from 'react-test-renderer';
 import PasswordInput from './PasswordInput';
 
 test('<PasswordInput /> renders correctly', () => {
-  const tree = renderer.create(<PasswordInput />).toJSON();
+  const tree = renderer.create(<PasswordInput
+    htmlId="password"
+    name="password"
+    onChange={() => {}}
+    value=""
+    minLength={8}
+    placeholder="Enter Password"
+    showVisibilityToggle
+    quality={0}
+    width={120}
+
+    />).toJSON();
   expect(tree).toMatchSnapshot();
 });
